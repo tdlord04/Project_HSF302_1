28d11fa69a30f680e5f85367e79a23671e2be8df
ChinhCDHE181478 <quansuper000@gmail.com>
2025-11-09 19:19:21 +0700

    commit

--- job_management_system/src/main/java/jms/service/JobPostingService.java
+++ job_management_system/src/main/java/jms/service/JobPostingService.java
@@ -3,6 +3,7 @@ package jms.service;
 import jms.dto.JobPostingDetailDto;
 import jms.dto.JobPostingDto;
 import jms.entity.enums.JobStatus;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -14,8 +15,9 @@ public interface JobPostingService {
     void delete(Long id);
     List<JobPostingDto> searchByTitle(String keyword);
     List<JobPostingDto> filterByStatus(JobStatus status);
-    List<JobPostingDto> advancedFilter(String title, String location, String salaryRange,
-                                       JobStatus status, String requirements, String companyName);
+    Page<JobPostingDto> advancedFilterPaged(String title, String location, String salaryRange,
+                                            JobStatus status, String requirements, String companyName,
+                                            int page, int size);
 
     JobPostingDetailDto getDetailById(Long id);
 }
