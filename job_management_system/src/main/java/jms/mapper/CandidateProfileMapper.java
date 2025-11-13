9743f148c331cff0ab445a26f6af836a5a529310
xuandat24 <Datpxhe186240@fpt.edu.vn>
2025-11-14 03:02:12 +0700

    Add files via upload

--- job_management_system/src/main/java/jms/mapper/CandidateProfileMapper.java
+++ job_management_system/src/main/java/jms/mapper/CandidateProfileMapper.java
@@ -7,21 +7,20 @@ import jms.entity.Skill;
 import jms.dto.SkillDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
+import java.util.Set;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface CandidateProfileMapper {
 
-    @Mapping(target = "skills", source = "skills", qualifiedByName = "mapSkills")
+    @Mapping(target = "skills", expression = "java(mapSkills(candidateProfile.getSkills()))")
     CandidateProfileDTO toDTO(CandidateProfile candidateProfile);
 
     CandidateProfile toEntity(CandidateProfileDTO candidateProfileDTO);
 
-    @Named("mapSkills")
-    default List<SkillDTO> mapSkills(List<Skill> skills) {
+    default List<SkillDTO> mapSkills(Set<Skill> skills) {
         if (skills == null) {
             return List.of();
         }
