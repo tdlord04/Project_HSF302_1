1fe739e2a6c25094cc674c8f3b57fa1346c34446
ChinhCDHE181478 <quansuper000@gmail.com>
2025-11-14 08:07:23 +0700

    commit

--- job_management_system/src/main/java/jms/repository/JobPostingRepository.java
+++ job_management_system/src/main/java/jms/repository/JobPostingRepository.java
@@ -18,16 +18,17 @@ public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
     List<JobPosting> findByJobTitleContainingIgnoreCase(String keyword);
 
     @Query("""
-                SELECT j FROM JobPosting j
-                LEFT JOIN j.company c
-                WHERE
-                    (:title IS NULL OR LOWER(j.jobTitle) LIKE LOWER(CONCAT('%', :title, '%')))
-                AND (:location IS NULL OR LOWER(j.location) LIKE LOWER(CONCAT('%', :location, '%')))
-                AND (:salaryRange IS NULL OR LOWER(j.salaryRange) LIKE LOWER(CONCAT('%', :salaryRange, '%')))
-                AND (:requirements IS NULL OR LOWER(j.requirements) LIKE LOWER(CONCAT('%', :requirements, '%')))
-                AND (:status IS NULL OR j.status = :status)
-                AND (:companyName IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', :companyName, '%')))
-            """)
+    SELECT j FROM JobPosting j
+    LEFT JOIN j.company c
+    WHERE
+        j.deletedAt IS NULL
+        AND (:title IS NULL OR LOWER(j.jobTitle) LIKE LOWER(CONCAT('%', :title, '%')))
+        AND (:location IS NULL OR LOWER(j.location) LIKE LOWER(CONCAT('%', :location, '%')))
+        AND (:salaryRange IS NULL OR LOWER(j.salaryRange) LIKE LOWER(CONCAT('%', :salaryRange, '%')))
+        AND (:requirements IS NULL OR LOWER(j.requirements) LIKE LOWER(CONCAT('%', :requirements, '%')))
+        AND (:status IS NULL OR j.status = :status)
+        AND (:companyName IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', :companyName, '%')))
+""")
     Page<JobPosting> advancedFilterPaged(
             @Param("title") String title,
             @Param("location") String location,
